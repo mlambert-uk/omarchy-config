885c8a558411fcc93931abbd3af73446fd226b6e
mark lambert <mark@mlambert.uk>
2025-11-10 13:55:32 +0000

    remove unwanted webapps

--- scripts/removeapps.sh
+++ scripts/removeapps.sh
@@ -2,3 +2,14 @@
 
 # Remove the default apps installed by Omarchy that are not wanted
 
+# Remove basecamp
+omarchy-webapp-remove Basecamp
+
+# Remove HEY
+omarchy-webapp-remove HEY
+
+# Remove whatsapp
+omarchy-webapp-remove WhatsApp
+
+# Remove X 
+omarchy-webapp-remove X
