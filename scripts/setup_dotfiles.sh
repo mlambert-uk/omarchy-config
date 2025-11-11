a8e09b1596335a9fc2dd58c0717c0d663621aaba
mark lambert <mark@mlambert.uk>
2025-11-11 08:45:33 +0000

    update stow commands

--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -3,14 +3,14 @@
 # Set up dotfiles using GNU Stow
 echo "Setting up dotfiles with Stow..."
 
-stow -d dotfiles -t $HOME ghostty --adopt
+stow -d dotfiles -t $HOME ghostty -R
 if [ $? -ne 0 ]; then
     echo "Error: Failed to stow ghostty configuration."
     exit 1
 fi
 
 # Set up waybar configuration
-stow -d dotfiles -t $HOME waybar --adopt
+stow -d dotfiles -t $HOME waybar -R
 if [ $? -ne 0 ]; then
     echo "Error: Failed to stow waybar configuration."
     exit 1
